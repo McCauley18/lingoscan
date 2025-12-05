5ee3413911c388df4927314a922474dda9b656bb
mccauley <mccauleyashu18@gmail.com>
2025-12-05 11:26:02 +0200

    Initial comment gitlab test

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="testtab"
+        options={{
+          title: 'Test nav',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="12.calendar" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
